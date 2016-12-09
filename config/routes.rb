c3be8b8c441e15f0635f8934b50266b065e785f3
Ryan <ryankawall01@gmail.com>
2016-12-09 18:04:12 +0000

    Added comment model, allow signed in users to create comments for existing grams

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "grams#index"
-  resources :grams, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :grams do
+    resources :comments, only: :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
